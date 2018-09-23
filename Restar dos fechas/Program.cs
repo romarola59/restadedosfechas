8b2a774f4b8f0b21fd72c69e34123f63854a1c47
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-22 22:45:16 -0300

    resta de dos fechas en dias

--- Restar dos fechas/Program.cs
+++ Restar dos fechas/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Restar_dos_fechas
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            DateTime fechaActual = DateTime.Now;  //  se inicia la variable facha actual
+            DateTime fechaNacimiento = new DateTime(1979, 9, 19);   //se crea la variable fecha de nacimiento que es la que se va a restar
+            TimeSpan diferencia = fechaActual.Subtract(fechaNacimiento);   // con este comando se hace la resta de la fecha actual a la fecha de nacimeinto.
+            // TimeSpan viene siendo como el intervalo de tiempo
+             
+            Console.WriteLine("Han pasado {0} dias",diferencia.Days);
+            Console.WriteLine("Si lo quieres con decimales: {0} dias", diferencia.TotalDays);
+            Console.WriteLine("Y si quieres con mas detalles {0} ", diferencia);
+            
+            Console.ReadKey();
+
         }
     }
 }
